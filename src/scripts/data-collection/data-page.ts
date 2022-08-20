564c5b8f4a16b30d92488bd328a86263338c986c
Exclusive-PG <dimchik_231@mail.ru>
2022-08-20 20:39:19 +0300

    refix circle progress bar

--- src/scripts/data-collection/data-page.ts
+++ src/scripts/data-collection/data-page.ts
@@ -1,52 +1,46 @@
 import { DataCollectionType } from "../types/types";
 import { dc } from "./DataCollection";
 
-const circle = document.querySelector<SVGCircleElement>(".circle-render");
-const cardNum = document.querySelector<HTMLDivElement>(".card__number");
-
-function circlePercent(percent: number, currentCircle?: SVGCircleElement, cardNumber?: HTMLDivElement) {
-	//let stroke
-	// currentCircle.style.strokeDashoffset = stroke.toString();
-	//cardNumber.textContent = `${percent.toFixed(2)} %`
-	return 565.49 - (565.49 * percent) / 100;
-}
-//circlePercent((dc.GetData.data.downloadedAllTimeSize/10000)*100,circle,cardNum)
-
-const MAX_VALUE = 1000;
-
 const arrayCards = [
-	{ title: "Current directory size", percent: 10000 / 10000 * 100 ,value : dc.GetData.data.currentDirectorySize  },
-	{ title: "Current files in directory", percent: dc.GetData.data.currentFilesInDirectory / 100 * 100 , value : dc.GetData.data.currentFilesInDirectory },
-	{ title: "Downloaded all time size", percent: dc.GetData.data.downloadedAllTimeSize / 10000 * 100 ,value :  dc.GetData.data.downloadedAllTimeSize },
+	{ title: "Current directory size", percent: (dc.GetData.data.currentDirectorySize / 10000) * 100, value: dc.GetData.data.currentDirectorySize },
+	{ title: "Current files in directory", percent: (dc.GetData.data.currentFilesInDirectory / 100) * 100, value: dc.GetData.data.currentFilesInDirectory },
+	{ title: "Downloaded all time size", percent: (dc.GetData.data.downloadedAllTimeSize / 10000) * 100, value: dc.GetData.data.downloadedAllTimeSize },
 ];
 
 const renderCircleCards = (arrayData: Array<any>, outerPlace: HTMLElement) => {
 	outerPlace.innerHTML = "";
-
 	arrayData.forEach((data: any) => {
 		outerPlace.innerHTML += `
-        <div class="card">
-        <div class="card__percent">
-            <svg>
-                <defs>
-                    <radialGradient id="gradient" cx="50%" cy="50%" r="60%" fx="50%" fy="50%">
-                        <stop offset="30%" stop-color="var(--primary-dark)" />
-                        <stop offset="100%" stop-color="var(--primary-light)" />
-                    </radialGradient>
-                </defs>
-                <circle cx="90" cy="90" r="90" stroke="url(#gradient)" class="circle-render" style="stroke-dashoffset:${circlePercent(data.percent)}";></circle>
-            </svg>
-            <div class="circle"></div>
-            <div class="circle circle__medium"></div>
-            <div class="circle circle__small"></div>
-            <div class="card__number">${parseInt(data.percent).toFixed(2).toString()} %</div>
+        <div class="circular_card">
+        <div class="circular-progress" style="background:${circleProgressBar(data.percent)}">
+            <div class="value-container">${data.percent.toFixed(2)} %</div>
         </div>
-        <div class="card__description">
-            <h2>${data.title}\n (${data.value.toFixed(0)})</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta cumque vitae iure voluptatem alias sed facilis quidem.</p>
-        </div>
+        <div class="title_circle_card">${data.title}</div>
     </div>`;
 	});
 };
 
-renderCircleCards(arrayCards, document.querySelector(".container-cards"));
+renderCircleCards(arrayCards, document.querySelector(".data_card_container"));
+
+function circleProgressBar(progressPercent: number) {
+	return `conic-gradient( #4d5bf9 ${progressPercent * 3.6}deg, #cadcff ${progressPercent * 3.6}deg ) `;
+}
+
+function circleProgressBar2(collectionCards: NodeListOf<HTMLElement>, data: Array<any>) {
+	let start = 0;
+	for (let index = 0; index < collectionCards.length; index++) {
+		let progress = setInterval(() => {
+			if (start >= data[index].percent) return;
+
+			start++;
+			collectionCards[index].style.background = `conic-gradient(
+      #4d5bf9 ${start * 3.6}deg,
+      #cadcff ${start * 3.6}deg
+  )`;
+		}, 50);
+	}
+}
+
+setTimeout(() => {
+	circleProgressBar2(document.querySelectorAll(".circular-progress"), arrayCards);
+}, 1000);
