20bf529e3c25a23d225af40bd801fe62031624e6
Exclusive-PG <dimchik_231@mail.ru>
2022-08-28 20:20:08 +0300

    added style for playlist

--- src/scripts/playlist/playlist.ts
+++ src/scripts/playlist/playlist.ts
@@ -38,27 +38,31 @@ for (let { playlist } of arrayPlaylist) {
 let result = Object.values(tempResult);
 console.log(result);
 
-const renderAvailablePlaylists = (outResult : HTMLElement) =>{
-    outResult.innerHTML = "";
+const renderAvailablePlaylists = (outResult: HTMLElement) => {
+	outResult.innerHTML = "";
 
-    result.forEach(({playlist,count}:any)=>{
-        outResult.innerHTML += `
+	result.forEach(({ playlist, count }: any) => {
+		outResult.innerHTML += `
         <div class="playlist_item id_playlist="${playlist}">
-            <div class="icon"><i class="fa-solid fa-folder fa-5x"></i></div>
-            <div class="data_playlist_item">${playlist.toUpperCase()}(${count})</div>
+            <span class="wrapper_playlist_item">
+                <div class="icon_playlist"><i class="fa-solid fa-folder fa-5x"></i></div>
+                <div class="data_playlist_item">${playlist.toUpperCase()}(${count})</div>
+            </span>
         </div>
-        `
-    })
-    document.querySelectorAll(".playlist_item").forEach((item:HTMLElement)=>{
-         item.addEventListener("mouseenter",()=>{
-            item.children[0].innerHTML =  `<i class="fa-regular fa-folder-open fa-5x"></i>`
-         })
-         
-         item.addEventListener("mouseleave",()=>{
-            item.children[0].innerHTML =  `<i class="fa-solid fa-folder fa-5x"></i>`
-         })
-      
-    })
-}
+        `;
+	});
+	document.querySelectorAll(".playlist_item").forEach((item: HTMLElement) => {
+		let timeout: NodeJS.Timeout;
+		clearTimeout(timeout);
+		item.addEventListener("mouseenter", () => {
+			item.children[0].children[0].innerHTML = `<i class="fa-regular fa-folder-open fa-5x"></i>`;
+		});
+		item.addEventListener("mouseleave", () => {
+			timeout = setTimeout(() => {
+				item.children[0].children[0].innerHTML = `<i class="fa-solid fa-folder fa-5x"></i>`;
+			}, 250);
+		});
+	});
+};
 
 renderAvailablePlaylists(playListsRender);
